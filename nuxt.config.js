21f33c4df18d2e8db9374f6112b671c6335a2bfc
MarkingCloud <genpokopokopoko@gmail.com>
2021-04-13 18:22:52 +0900

    feat(config): フォーマット修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,9 +30,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    { src: '~/plugins/localStorage.js', ssr: false }
-  ],
+  plugins: [{ src: '~/plugins/localStorage.js', ssr: false }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
